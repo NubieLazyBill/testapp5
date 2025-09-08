8817e7a6e1e2fa23d004b03f22d63211982b03fc
Lazy Bill <lazybill2580@gmail.com>
2025-09-08 13:14:36 +0500

    тест апп5

---
+++ app/src/main/java/com/example/testapp5/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testapp5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // Просто установка макета
+    }
+}
